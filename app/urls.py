8b383047db3843d75eb0434d3c324d19cf3f2dc8
bhanufyi <bhanuprasadcherukuvada2000@gmail.com>
2024-08-26 23:41:35 -0400

    feat(urls): remove index view to avoid serving react app from django

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from app.views import home, get_video_data, leg_raise_task, updatePlotData, update_landmarks
+from app.views import get_video_data, leg_raise_task, updatePlotData, update_landmarks
 
 urlpatterns = [
-    path('', home),
     path('video/', get_video_data),
     path('leg_raise/', leg_raise_task),
     path('update_plot/', updatePlotData),
